f12f3b877a87bc7079de91386aee1f3db1d40913
Dario Piotrowicz <dario@cloudflare.com>
2024-01-19 11:36:07 +0000

    use the getBindingsProxy generic format

--- demo/index.ts
+++ demo/index.ts
@@ -1,25 +1,28 @@
-import { getBindingsProxy } from 'wrangler';
-import type { DurableObjectNamespace, KVNamespace } from '@cloudflare/workers-types';
+import { getBindingsProxy } from "wrangler";
+import type {
+  DurableObjectNamespace,
+  KVNamespace,
+} from "@cloudflare/workers-types";
 
-const {
-    bindings,
-    dispose
-} = await getBindingsProxy({});
+const { bindings, dispose } = await getBindingsProxy<{
+  MY_KV_FROM_TOML: KVNamespace;
+  MY_DO: DurableObjectNamespace;
+}>();
 
-const myKv = bindings['MY_KV_FROM_TOML'] as KVNamespace;
+const myKv = bindings["MY_KV_FROM_TOML"];
 
 const numOfKvEntries = (await myKv.list()).keys.length;
 
-const myDo = bindings['MY_DO'] as DurableObjectNamespace;
+const myDo = bindings["MY_DO"];
 
-const doId = myDo.idFromName('my-do-name');
+const doId = myDo.idFromName("my-do-name");
 const doObj = myDo.get(doId);
-const resp = await doObj.fetch('https://0.0.0.0');
+const resp = await doObj.fetch("https://0.0.0.0");
 const txt = await resp.text();
 
 console.log(`
 
-    MY_VARIABLE = ${bindings['MY_VARIABLE_FROM_TOML']}
+    MY_VARIABLE = ${bindings["MY_VARIABLE_FROM_TOML"]}
 
     # kv entries in MY_KV_FROM_TOML = ${numOfKvEntries}
 
